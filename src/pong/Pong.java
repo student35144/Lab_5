6aa16b463aa37678c741508196bff7a19e08ce11
deSp44 <michalcz97@gmail.com>
2021-05-29 14:59:40 +0200

    Zadanie nr.2

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -10,16 +10,35 @@ import javafx.stage.Stage;
 
 public class Pong extends Application {
 
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 600;
+    private static final double MARGIN = 50;
+    private static final double MARGINZERO = 0;
+    private static final double ARENAWIDTH = WIDTH - 2*MARGIN;
+    private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;;
+    private static final double ARENAX1 = MARGIN;
+    private static final double ARENAY1 = MARGIN;
+    private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
+    private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
+    private static final double R = 10;
+
+
     @Override
     public void start(Stage stage) {
-        Canvas canvas = new Canvas(800, 600);
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
+        gc.setFill(Color.DARKRED);
+        gc.fillRect(MARGINZERO, MARGINZERO, WIDTH, HEIGHT);
+
         gc.setFill(Color.BLACK);
-        gc.fillRect(50, 50, 700, 500);
+        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+
+        gc.setFill(Color.DARKOLIVEGREEN);
+        gc.fillOval(ARENAX1+ARENAWIDTH/2,ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
     }
 
     public static void main(String[] args) {
