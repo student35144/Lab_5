8846d3342069b45a34db51b4eb7a4468efb3bd62
deSp44 <michalcz97@gmail.com>
2021-05-29 15:12:55 +0200

    Zadanie nr.3

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -1,5 +1,7 @@
 package pong;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -7,6 +9,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Pong extends Application {
 
@@ -22,26 +25,38 @@ public class Pong extends Application {
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R = 10;
 
+    private double x = ARENAX1+ARENAWIDTH/2;
+
 
     @Override
     public void start(Stage stage) {
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(50), actionEvent -> run(gc)));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-        gc.setFill(Color.DARKRED);
-        gc.fillRect(MARGINZERO, MARGINZERO, WIDTH, HEIGHT);
+        timeline.play();
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
 
-        gc.setFill(Color.DARKOLIVEGREEN);
-        gc.fillOval(ARENAX1+ARENAWIDTH/2,ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void run (GraphicsContext graphicsContext){
+        graphicsContext.setFill(Color.DARKRED);
+        graphicsContext.fillRect(MARGINZERO, MARGINZERO, WIDTH, HEIGHT);
+
+        graphicsContext.setFill(Color.BLACK);
+        graphicsContext.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+
+        graphicsContext.setFill(Color.DARKOLIVEGREEN);
+        graphicsContext.fillOval(x += 5,ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
     }
 }
